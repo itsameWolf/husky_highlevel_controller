828e8385f76b32e0f257477a458f02a3b58750f3
itsameWolf <lupo961@gmail.com>
2020-03-02 11:49:18 +0000

    start exercise 3

--- include/husky_highlevel_controller/HuskyHighlevelController.hpp
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -2,7 +2,10 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
 #include <string>
+
+#define pi 3.14159265349
 
 using std::string;
 
@@ -11,24 +14,34 @@ namespace husky_highlevel_controller {
 /*!
  * Class containing the Husky Highlevel Controller
  */
-class HuskyHighlevelController {
-public:
-	/*!
-	 * Constructor.
-	 */
-	HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+    class HuskyHighlevelController {
+    public:
+        /*!
+         * Constructor.
+         */
+        HuskyHighlevelController(ros::NodeHandle &nodeHandle);
 
-	/*!
-	 * Destructor.
-	 */
-	virtual ~HuskyHighlevelController();
+        /*!
+         * Destructor.
+         */
+        virtual ~HuskyHighlevelController();
 
-private:
-	ros::NodeHandle nodeHandle_;
-	void laserScanMsg(const sensor_msgs::LaserScan &msg);
-	ros::Subscriber laser_msg;
-	int queue_size;
-	string topic_name;
-};
+    private:
+        ros::NodeHandle nodeHandle_;
+        ros::Subscriber laser_msg;
+        ros::Publisher vel_pub;
+
+        void laserScanMsg(const sensor_msgs::LaserScan &msg);
+
+        int queue_size;
+        string topic_name;
+
+        float min = INFINITY;
+        auto min_i = 0;
+        float pillar_angle = pi / 2;
+        float speed = 2;
+
+        geometry_msg::Twist cmd_vel_command;
+    };
 
 } /* namespace */
--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -3,36 +3,41 @@
 
 namespace husky_highlevel_controller {
 
-HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
-  nodeHandle_(nodeHandle)
-{
-    nodeHandle.getParam("/husky_highlevel_controller/husky/topic_name", topic_name);
-    nodeHandle.getParam("/husky_highlevel_controller/husky/queue_size", queue_size);
-    ROS_INFO_STREAM(topic_name << " " << queue_size);
-    laser_msg = nodeHandle.subscribe(topic_name, queue_size, &HuskyHighlevelController::laserScanMsg, this);
-}
+    HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle &nodeHandle) :
+            nodeHandle_(nodeHandle) {
+        nodeHandle.getParam("/husky_highlevel_controller/husky/topic_name", topic_name);
+        nodeHandle.getParam("/husky_highlevel_controller/husky/queue_size", queue_size);
+        ROS_INFO_STREAM(topic_name << " " << queue_size);
 
-HuskyHighlevelController::~HuskyHighlevelController()
-{
-}
+        laser_msg = nodeHandle.subscribe(topic_name, queue_size, &HuskyHighlevelController::laserScanMsg, this);
 
-void HuskyHighlevelController::laserScanMsg(const sensor_msgs::LaserScan &msg)
-{
-    float min = INFINITY;
-    auto len = msg.ranges.size();
-
-    for (auto i = 0; i < len; i++)
-    {
-        if (min==INFINITY)
-        {
-            min = msg.ranges[i];
-        }
-        if (msg.ranges[i] < min)
-        {
-            min = msg.ranges[i];
-        }
-        ROS_INFO_STREAM("Minimum laser reading: "<<min);
+        vel_pub = nodeHandle.advertise<geometry_msg::Twist_msg>("/cmd_vel", 10);
     }
-}
+
+    HuskyHighlevelController::~HuskyHighlevelController() {
+    }
+
+    void HuskyHighlevelController::laserScanMsg(const sensor_msgs::LaserScan &msg) {
+
+        auto len = msg.ranges.size();
+
+        for (auto i = 0; i < len; i++) {
+            if (min == INFINITY) {
+                min = msg.ranges[i];
+            }
+            if (msg.ranges[i] < min) {
+                min = msg.ranges[i];
+                min_i = i;
+            }
+            ROS_INFO_STREAM("Minimum laser reading: " << min);
+        }
+
+        pillar_angle = -0.875 + 1.5 * pi / 720 * min_i;
+
+        cmd_vel_command.linear.x = speed;
+        cmd_vel_command.angular.z = 0.5 * (0-(pillar_angle-pi/2);
+
+        vel_pub.publish(cmd_vel_command);
+    }
 
 } /* namespace */
